5a85e15cf44ed620ad0623ef5100665ce52ed4b9
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2025-07-30 14:57:52 +0200

    chore: setup rn testing lib eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,12 @@ const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 const tanstackConfig = require("@tanstack/eslint-plugin-query");
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const testingLibrary = require("eslint-plugin-testing-library");
 
 module.exports = defineConfig([
   expoConfig,
   tanstackConfig.configs["flat/recommended"],
+  testingLibrary.configs["flat/react"],
   eslintPluginPrettierRecommended,
   {
     ignores: ["dist/*", "expo-env.d.ts"],
